d2df85447932bc44fb497ccdc3f5549d9deb4790
Draxy <Draxy@Draxy>
2020-09-04 16:04:12 +1000

    changed index.jsp to display login page. Also added a login servlet file which you are redirected to once login details are entered and submitted. Once you get redirected to the page it runs a basic validation on the values sent from the login details on the previous page.

--- WebContent/index.jsp
+++ WebContent/index.jsp
@@ -4,9 +4,14 @@
 <html>
 <head>
 <meta charset="ISO-8859-1">
-<title>Insert title here</title>
+<title>Login</title>
 </head>
 <body>
-	<h1>this is the index landing page</h1>
+		<h1>Please Login to View Dashboard</h1>
+		<form action="dashboard" method="post">
+		Username: <input type="text" name="username" width="30"/>
+		Password: <input type="password" name="password" width="10"/>
+		<input type="submit" value="Login"/>
+		</form>
 </body>
 </html>
---
+++ src/webapp/Login.java
@@ -0,0 +1,49 @@
+package webapp;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Login
+ */
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Login() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+		out.println("You have logged in with username: "+ request.getParameter("username")+" and password: "+request.getParameter("password"));
+		if ((request.getParameter("username").equalsIgnoreCase("abc") && (request.getParameter("password").equals("123")))) {
+			out.print("Success");
+		} else {
+			out.print("Fail");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
